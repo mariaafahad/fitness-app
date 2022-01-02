784a011a0ef81eb173b271429315d719c6e1f559
Maria Kamal <mariaafahad@gmail.com>
2022-01-02 22:36:42 +0600

    h1 added

--- src/Pages/Homepage/Home/Home.js
+++ src/Pages/Homepage/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import About from '../../../Components/About/About';
 import HeroSection from '../HeroSection/HeroSection';
 
 const Home = () => {
     return (
         <>
             <HeroSection></HeroSection>
+            <About></About>
         </>
     );
 };
