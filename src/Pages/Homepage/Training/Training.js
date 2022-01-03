4c6410da6953557e1560dba0d16ac6acd6e432ff
Maria Kamal <mariaafahad@gmail.com>
2022-01-03 13:32:38 +0600

    program added

--- src/Pages/Homepage/Training/Training.js
+++ src/Pages/Homepage/Training/Training.js
@@ -2,45 +2,38 @@ import React from 'react';
 
 const Training = () => {
     return (
-        <div className="container m-20 md:flex md:justify-center md:space-x-8 md:px-14">
-            <div class="wrapper bg-gray-400 antialiased text-gray-900">
-                <div>
+        <div>
+            <h1 className="text-4xl font-bold text-neutral-800">Our Training Program</h1>
+            <section class=" container flex max-w-7xl mx-auto px-4 sm:px-6 lg:px-4 mb-12">
 
-                    <img src="https://i.ibb.co/2kTSTTY/1000-F-286305748-Vy5ed2f-Nnsw-FQa-SGINykodj-GBZ8ey1-ZJ.jpg" alt=" random imgee" class="w-full object-cover object-center rounded-lg shadow-md" />
 
-                    <div class="relative px-4 -mt-16  ">
-                        <div class="bg-white p-6 rounded-lg shadow-lg">
-                            <h4 class="mt-1 text-xl font-semibold uppercase leading-tight truncate">Yoga</h4>
-
-                            <div class="mt-1">
-                                $180
-                                <span class="text-gray-600 text-sm">   /month</span>
-                            </div>
+                <div class="relative group w-96 h-96 overflow-hidden bg-black m-auto mt-36">
+                    <img class="object-cover w-full h-full transform duration-700 backdrop-opacity-100" src="https://i.ibb.co/2kTSTTY/1000-F-286305748-Vy5ed2f-Nnsw-FQa-SGINykodj-GBZ8ey1-ZJ.jpg" alt="" />
+                    <div class="absolute w-full h-full shadow-2xl opacity-20 transform duration-500 inset-y-full group-hover:-inset-y-0"></div>
+                    <div class="absolute bg-gradient-to-t from-black w-full h-full transform duration-500 inset-y-3/4 group-hover:-inset-y-0">
+                        <div class="absolute w-full flex place-content-center">
+                            <p class="capitalize font-serif font-bold text-3xl text-center shadow-2xl text-white mt-10">Yoga Program</p>
                         </div>
+                        <div class="absolute w-full flex place-content-center mt-20">
+                            <p class="font-sans text-center w-4/5 text-white mt-5">Collaborative effort of a group to achieve a common goal the most effective and efficient way!</p>
+                        </div>
+                        <button class="absolute left-1/4 bottom-4 bg-white text-black font-bold rounded-lg h-10 w-48">Contact Us</button>
                     </div>
                 </div>
-            </div>
-            <div class="wrapper bg-gray-400 antialiased text-gray-900">
-                <div>
-
-                    <img src="https://i.ibb.co/nCCM2Tj/1000-F-317917629-Hj-BCy-Rl-H1-Hpwwg2-Hf-Eb-Ex-Tdkby-Wi-GFu-N.jpg" alt=" random imgee" class="w-full object-cover object-center rounded-lg shadow-md" />
-
-                    <div class="relative px-4 -mt-16  ">
-                        <div class="bg-white p-6 rounded-lg shadow-lg">
-
-
-                            <h4 class="mt-1 text-xl font-semibold uppercase leading-tight truncate">Personal Trainer</h4>
-
-                            <div class="mt-1">
-                                $300
-                                <span class="text-gray-600 text-sm">   /month</span>
-                            </div>
-
+                <div class="relative group w-96 h-96 overflow-hidden bg-black m-auto mt-36">
+                    <img class="object-cover w-full h-full transform duration-700 backdrop-opacity-100" src="https://i.ibb.co/nCCM2Tj/1000-F-317917629-Hj-BCy-Rl-H1-Hpwwg2-Hf-Eb-Ex-Tdkby-Wi-GFu-N.jpg" alt="" />
+                    <div class="absolute w-full h-full shadow-2xl opacity-20 transform duration-500 inset-y-full group-hover:-inset-y-0"></div>
+                    <div class="absolute bg-gradient-to-t from-black w-full h-full transform duration-500 inset-y-3/4 group-hover:-inset-y-0">
+                        <div class="absolute w-full flex place-content-center">
+                            <p class="capitalize font-serif font-bold text-3xl text-center shadow-2xl text-white mt-10">Personal Program</p>
                         </div>
+                        <div class="absolute w-full flex place-content-center mt-20">
+                            <p class="font-sans text-center w-4/5 text-white mt-5">Collaborative effort of a group to achieve a common goal the most effective and efficient way!</p>
+                        </div>
+                        <button class="absolute left-1/4 bottom-4 bg-white text-black font-bold rounded-lg h-10 w-48">Contact Us</button>
                     </div>
-
                 </div>
-            </div>
+            </section>
         </div>
     );
 };
