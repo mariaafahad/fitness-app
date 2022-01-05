2345564b54d39c67a701c4be141af934bf4de0a8
Shakawat Hossain <mailshakawatii@gmail.com>
2022-01-05 16:03:36 +0600

    required feild

---
+++ src/Pages/Dashboard/ClientReviews/ClientReviews.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ClientReviews = () => {
+    return (
+        <div>
+            <h3 className="text-4xl">Client Review</h3>
+        </div>
+    );
+};
+
+export default ClientReviews;
--- src/Pages/Homepage/Home/Home.js
+++ src/Pages/Homepage/Home/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Footer from "../../../Components/Footer/Footer";
 import Header from "../../../Components/Header/Header";
+import Contact from "../Contact/Contact";
 import Features from "../Features/Features";
 import HeroSection from "../HeroSection/HeroSection";
 import Specialities from "../Specialities/Specialities";
@@ -14,7 +15,7 @@ const Home = () => {
       <Features></Features>
       <Training></Training>
       <Specialities></Specialities>
-
+        <Contact></Contact>
       <Footer></Footer>
     </>
   );
